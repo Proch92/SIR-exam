15d476cda84972bf4bfa1cc85a3aadf01fad9bea
Michele Proverbio <proch92@gmail.com>
2019-06-03 18:30:28 +0200

    transforming gates into a logic table

--- project/bool_nk_net/gates.py
+++ project/bool_nk_net/gates.py
@@ -1,3 +1,5 @@
+import itertools
+
 gates = [
     lambda ins: ins[0] and ins[1],
     lambda ins: ins[0] or ins[1],
@@ -10,3 +12,5 @@ gates = [
     lambda ins: not ins[0],
     lambda ins: not ins[1],
 ]
+
+boolean_matrix = list(itertools.product([0, 1], repeat=1))
